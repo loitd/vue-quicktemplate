8df12aff2dcf4dbd75ada3e7da278ee3bcae2b1c
loitd <loitranduc@gmail.com>
2019-02-13 23:20:02 +0700

    'deploy-minor-fixes'

--- src/components/LineChartCard.vue
+++ src/components/LineChartCard.vue
@@ -1,5 +1,27 @@
 <template>
-    <h1>Haha</h1>
+    <div>
+        <b-card-group deck>
+            <b-card header="featured"
+                    header-tag="header"
+                    footer="Card Footer"
+                    footer-tag="footer"
+                    title="Title">
+                <p class="card-text">Header and footers using props.</p>
+                <b-button href="#"
+                        variant="primary">Go somewhere</b-button>
+            </b-card>
+            <b-card title="Title"
+                    header-tag="header"
+                    footer-tag="footer">
+                <h6 slot="header"
+                    class="mb-0">Header Slot</h6>
+                <em slot="footer">Footer Slot</em>
+                <p class="card-text">Header and footers using slots.</p>
+                <b-button href="#"
+                        variant="primary">Go somewhere</b-button>
+            </b-card>
+        </b-card-group>
+    </div>
 </template>
 
 <script>
