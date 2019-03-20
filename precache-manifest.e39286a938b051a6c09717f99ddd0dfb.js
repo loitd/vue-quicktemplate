5500d82fb9fed19cad4ec1e636fb77a95ff44438
loitd <loitranduc@gmail.com>
2019-03-20 14:36:21 +0700

    'deploy-git-minor-fixes'

---
+++ precache-manifest.e39286a938b051a6c09717f99ddd0dfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-quick-template/robots.txt"
+  },
+  {
+    "revision": "12aa753ffcb33ecc7300",
+    "url": "/vue-quick-template/js/chunk-vendors.2117e351.js"
+  },
+  {
+    "revision": "7671e0848e446681675f",
+    "url": "/vue-quick-template/js/app.4388b910.js"
+  },
+  {
+    "revision": "f526fc0d9fd2b1a1c6e5",
+    "url": "/vue-quick-template/js/about.281796b6.js"
+  },
+  {
+    "revision": "17705e7350ac64897754a01b2e5334e3",
+    "url": "/vue-quick-template/index.html"
+  },
+  {
+    "revision": "12aa753ffcb33ecc7300",
+    "url": "/vue-quick-template/css/chunk-vendors.1b8a695f.css"
+  },
+  {
+    "revision": "7671e0848e446681675f",
+    "url": "/vue-quick-template/css/app.e7a3c6fb.css"
+  }
+];
